1c9042f4ef9952df69e6a4ddd857649becf99d2f
sadicangel <sadicangel@hotmail.com>
2024-03-19 18:02:12 +0000

    Make count button reactive.

--- src/main.go
+++ src/main.go
@@ -33,8 +33,11 @@ func main() {
 
 	count := Count{Count: 0}
 	e.GET("/", func(c echo.Context) error {
-		count.Count++
 		return c.Render(200, "index", count)
+	})
+	e.POST("count", func(c echo.Context) error {
+		count.Count++
+		return c.Render(200, "count", count)
 	})
 	e.Logger.Fatal(e.Start(":3000"))
 }
