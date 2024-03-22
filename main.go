684e51950bdb4739d101373f80cc6c20495f433b
sadicangel <sadicangel@hotmail.com>
2024-03-22 19:28:09 +0000

    Adds details page.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"io"
 	"os"
@@ -18,11 +19,30 @@ func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Co
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
+const MAX = 151
+
 type Pokemon struct {
 	Id     int      `json: "id"`
 	Name   string   `json: "name"`
 	Types  []string `json: "types"`
 	Sprite string   `json: "sprite`
+}
+
+type PokemonStats struct {
+	HP        int `json: "HP"`
+	Attack    int `json: "Attack"`
+	Defense   int `json: "Defense"`
+	SpAttack  int `json: "SpAttack"`
+	SpDefense int `json: "SpDefense"`
+	Speed     int `json: "Speed"`
+}
+
+type PokemonDetails struct {
+	Id    int          `json: "id"`
+	Name  string       `json: "name"`
+	Types []string     `json: "types"`
+	Stats PokemonStats `json: "stats"`
+	Image string       `json: "image`
 }
 
 func readPokemonList() []Pokemon {
@@ -37,7 +57,26 @@ func readPokemonList() []Pokemon {
 
 	json.Unmarshal(pokemonBytes, &pokemonList)
 
-	return pokemonList[0:151]
+	return pokemonList[0:MAX]
+}
+
+func readPokemonDetails() map[string]PokemonDetails {
+	var pokemonList []PokemonDetails
+	pokemonJson, err := os.Open("data/pokemon_details.json")
+	if err != nil {
+		panic(err)
+	}
+	defer pokemonJson.Close()
+
+	pokemonBytes, _ := io.ReadAll(pokemonJson)
+
+	json.Unmarshal(pokemonBytes, &pokemonList)
+
+	details := make(map[string]PokemonDetails)
+	for i := 0; i < MAX; i++ {
+		details[fmt.Sprint(pokemonList[i].Id)] = pokemonList[i]
+	}
+	return details
 }
 
 func main() {
@@ -46,12 +85,17 @@ func main() {
 	e.Renderer = &Templates{
 		templates: template.Must(template.ParseGlob("views/*.html")),
 	}
-	e.Static("/", "public")
+	e.Static("/public", "public")
 
 	pokemonList := readPokemonList()
+	pokemonDetails := readPokemonDetails()
 
 	e.GET("/", func(c echo.Context) error {
 		return c.Render(200, "index", pokemonList)
+	})
+	e.GET("/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		return c.Render(200, "details", pokemonDetails[id])
 	})
 	e.Logger.Fatal(e.Start(":3000"))
 }
